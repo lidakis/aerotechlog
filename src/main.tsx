bafc4ebf281d97c87d12a2860742beed9e61d875
lidakis <lidakis@gmail.com>
2026-01-19 07:42:30 +0200

    Add basename to BrowserRouter for GitHub Pages
    React Router needs the basename prop to properly handle routing
    when deployed to a subdirectory on GitHub Pages.

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
       <HelmetProvider>
-        <BrowserRouter>
+        <BrowserRouter basename="/aerotechlog">
           <App />
         </BrowserRouter>
       </HelmetProvider>
